404d693439c8e8b3f0a969f8828ffac9ec581d10
ankit-mogha <ankit.mogha@knoldus.com>
2022-07-30 11:33:14 +0530

    Initial commit

---
+++ src/main/java/io/knoldus/SpringBootWebApplicationBootstrapJqueryApplication.java
@@ -0,0 +1,12 @@
+package io.knoldus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplicationBootstrapJqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplicationBootstrapJqueryApplication.class, args);
+	}
+}
